015abec4b77c7bd700203b51f43248fd12bbdeee
kindiana <wangben3@gmail.com>
2018-06-27 19:09:59 +0800

    fix backwards compat with alpha 4

--- rita/src/rita_common/network_endpoints/mod.rs
+++ rita/src/rita_common/network_endpoints/mod.rs
@@ -48,7 +48,29 @@ pub fn make_payments(
 }
 
 pub fn hello_response(
-    their_id: Json<Identity>,
+    their_id: Json<serde_json::Value>,
+    req: HttpRequest,
+) -> Box<Future<Item = Json<LocalIdentity>, Error = Error>> {
+    let new_id: Result<Identity, serde_json::Error> = serde_json::from_value(their_id.clone());
+    let old_id: Result<LocalIdentity, serde_json::Error> = serde_json::from_value(their_id.clone());
+
+    match new_id {
+        Ok(new_id) => {
+            info!("sending to new hello handler");
+            hello_response_new(new_id, req)
+        }
+        Err(_) => match old_id {
+            Ok(old_id) => {
+                info!("sending to old hello handler");
+                hello_response_old(old_id, req)
+            }
+            _ => panic!("did not match either"),
+        },
+    }
+}
+
+pub fn hello_response_new(
+    their_id: Identity,
     req: HttpRequest,
 ) -> Box<Future<Item = Json<LocalIdentity>, Error = Error>> {
     info!("Got Hello from {:?}", req.connection_info().remote());
@@ -62,6 +84,33 @@ pub fn hello_response(
             info!("opening tunnel in hello_response for {:?}", their_id);
             TunnelManager::from_registry()
                 .send(GetWgInterface(their_id.mesh_ip))
+                .from_err()
+                .and_then(|wg_iface| {
+                    Ok(Json(LocalIdentity {
+                        global: SETTING.get_identity(),
+                        wg_port: wg_iface?.listen_port,
+                    }))
+                })
+        })
+        .responder()
+}
+
+// TODO: REMOVE IN ALPHA 6
+pub fn hello_response_old(
+    their_id: LocalIdentity,
+    req: HttpRequest,
+) -> Box<Future<Item = Json<LocalIdentity>, Error = Error>> {
+    info!("Got Hello from {:?}", req.connection_info().remote());
+
+    trace!("Received neighbour identity: {:?}", their_id);
+
+    TunnelManager::from_registry()
+        .send(OpenTunnelListener(their_id.clone().global))
+        .from_err()
+        .and_then(move |_| {
+            info!("opening tunnel in hello_response for {:?}", their_id);
+            TunnelManager::from_registry()
+                .send(GetWgInterface(their_id.global.mesh_ip))
                 .from_err()
                 .and_then(|wg_iface| {
                     Ok(Json(LocalIdentity {
